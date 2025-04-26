7eb815fbe3c1aa217bfba24de5d71b2bdf6be73b
Jesse B. Miller <jesse@jessebmiller.com>
2025-04-26 09:42:59 -0500

    add gamefeel controls

--- games/platformer-gamefeel-sandbox/src/main.ts
+++ games/platformer-gamefeel-sandbox/src/main.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { setupDesignerUI } from './designerUI';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -27,6 +28,17 @@ let player!: Phaser.Physics.Arcade.Sprite;
 let cursors: any;
 let pad: Phaser.Input.Gamepad.Gamepad | null = null;
 
+// Gamefeel parameters (designer adjustable)
+let gravity = 1000;
+let jumpStrength = -500;
+let moveSpeed = 200;
+let coyoteTimeMs = 120; // milliseconds
+
+// Coyote time state
+let coyoteTimer = 0;
+let wasOnGround = false;
+let prevJumpPressed = false;
+
 function preload(this: Phaser.Scene) {
   // Placeholder: use a simple rectangle for the player
 }
@@ -36,6 +48,9 @@ function create(this: Phaser.Scene) {
   player = this.physics.add.sprite(400, 300, '');
   player.setDisplaySize(40, 60);
   player.setCollideWorldBounds(true);
+
+  // Set initial gravity
+  this.physics.world.gravity.y = gravity;
 
   // Create ground
   const ground = this.add.rectangle(400, 580, 800, 40, 0x888888);
@@ -67,43 +82,71 @@ function create(this: Phaser.Scene) {
   });
   console.log(result);
   console.log(this.input.gamepad);
+
+  setupDesignerUI(this, {
+    gravity: () => gravity,
+    jumpStrength: () => jumpStrength,
+    moveSpeed: () => moveSpeed,
+    coyoteTimeMs: () => coyoteTimeMs,
+  }, {
+    setGravity: v => { gravity = v; },
+    setJumpStrength: v => { jumpStrength = v; },
+    setMoveSpeed: v => { moveSpeed = v; },
+    setCoyoteTimeMs: v => { coyoteTimeMs = v; },
+  });
 }
 
-function update(this: Phaser.Scene) {
+function update(this: Phaser.Scene, time: number, delta: number) {
+  // Track if player is on ground
+  const onGround = !!(player.body && (player.body as Phaser.Physics.Arcade.Body).touching.down);
+
+  // Coyote time logic
+  if (onGround) {
+    coyoteTimer = coyoteTimeMs;
+  } else if (coyoteTimer > 0) {
+    coyoteTimer -= delta;
+  }
+
   // Keyboard controls
+  let jumpPressed = false;
   if (cursors) {
     if (cursors.left.isDown) {
-      player.setVelocityX(-200);
+      player.setVelocityX(-moveSpeed);
     } else if (cursors.right.isDown) {
-      player.setVelocityX(200);
+      player.setVelocityX(moveSpeed);
     } else {
       player.setVelocityX(0);
     }
-    if (cursors.up.isDown && player.body && (player.body as Phaser.Physics.Arcade.Body).touching.down) {
-      player.setVelocityY(-500);
-    }
+    jumpPressed = cursors.up.isDown;
   }
 
   // Gamepad controls
   if (pad) {
-    // Left stick X axis
     const axisH = pad.axes.length > 0 ? pad.axes[0].getValue() : 0;
     if (axisH < -0.1) {
-      player.setVelocityX(-200);
+      player.setVelocityX(-moveSpeed);
     } else if (axisH > 0.1) {
-      player.setVelocityX(200);
+      player.setVelocityX(moveSpeed);
     } else {
       player.setVelocityX(0);
     }
-    // A button (index 0) for jump
-    if (
-      pad.buttons[0].pressed &&
-      player.body &&
-      (player.body as Phaser.Physics.Arcade.Body).touching.down
-    ) {
-      player.setVelocityY(-500);
+    if (pad.buttons[0].pressed) {
+      jumpPressed = true;
     }
   }
+
+  // Jump logic with coyote time (edge detection)
+  if (
+    jumpPressed &&
+    !prevJumpPressed && // just pressed
+    coyoteTimer > 0 &&
+    player.body
+  ) {
+    player.setVelocityY(jumpStrength);
+    coyoteTimer = 0;
+  }
+  prevJumpPressed = jumpPressed;
+  wasOnGround = onGround;
 }
 
 new Phaser.Game(config); 
