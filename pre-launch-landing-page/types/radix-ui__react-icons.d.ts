bf021b8689a01027e23928436931936734523d2c
Jesse B. Miller <jesse@jessebmiller.com>
2025-04-30 15:21:31 -0500

    builds now

--- pre-launch-landing-page/types/radix-ui__react-icons.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@radix-ui/react-icons' {
-  import { ComponentProps } from 'react'
-  
-  interface IconProps extends ComponentProps<'svg'> {
-    className?: string
-  }
-  
-  export const UpdateIcon: React.FC<IconProps>
-} 
